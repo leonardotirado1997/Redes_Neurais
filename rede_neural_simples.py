1131ee41cd6abed8bd9125368da1a9f9a9dba5f5
leonardotirado1997 <leopirestirado1997@gmail.com>
2026-06-08 21:53:04 -0300

    Aplicando soma ponderada e aplicando o calculo juntado

--- rede_neural_simples.py
+++ rede_neural_simples.py
@@ -11,7 +11,16 @@ frequencia = float(input("Informe a frequência do aluno (%): \n"))
 # Pesos das entradas
 # Quanto maior o peso, maior a influência dessa variável na decisão final
 peso_nota = 0.7
-peso_frequencia = 0/3
+peso_frequencia = 0.3
 
 print(f"Nota {nota} , Peso nota: {peso_nota}")
 print(f"Frequencia {frequencia} Peso Frequência: {peso_frequencia}")
+
+# Soma Ponderada
+# Multiplica cada entrada pelo seu peso e soma os resultados
+
+resultado = (
+    nota * peso_nota + frequencia * peso_frequencia
+)
+
+print(f"Resultado calculado: {resultado}")
